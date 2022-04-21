9f5d7680a1b738b69f615d0d6474712291324806
a-Aram-a <aramtashchan@gmail.com>
2022-04-21 19:09:54 +0300

    Added registration invite code2

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -5,8 +5,12 @@ import io.ktor.server.netty.*
 import com.example.plugins.*
 import com.example.models.Param
 import com.example.models.Script
+import java.io.File
 
 fun main() {
+    if(!(File("invitecode.txt").exists())) {
+        File("invitecode.txt").writeText("termoPhysics_g9xB*5")
+    }
     embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
         configureRouting()
         configureTemplating()
