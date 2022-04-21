38a30a6c81775f4133e21fa32d7673aca2e926a9
a-Aram-a <aramtashchan@gmail.com>
2022-04-21 20:25:14 +0300

    Tabs implement v0.1

--- src/main/resources/files/scripts/home.js
+++ src/main/resources/files/scripts/home.js
@@ -32,59 +32,32 @@ function printScripts(scriptsArray) {
     let horizontalDivArray = [];
     let goToScriptBtn;
     let cardDiv;
-    let rowDiv
+    let tabsWrapper
     let titleP;
     let descP;
 
     let scriptId;
     let textOfDesc;
 
+    tabsWrapper = document.getElementById("tabsWrapper");
     for (let i = 0; i < scriptsArray.length; i++) {
 
         scriptId = scriptsArray[i].id
         textOfDesc = scriptsArray[i].description.length === 0 ? " - " : scriptsArray[i].description
 
         cardDiv = createElement('div', {
-            class: 'rowWithSpace scriptCard'
+            class: 'rowWithSpace scriptCard',
+            onclick: "goToScript(" + scriptId + ")",
+            title: textOfDesc
         });
         titleP = createElement("h3", {
             innerText: scriptsArray[i].name
         });
-        descP = createElement("p", {
-            innerText: (textOfDesc.length > 50) ? textOfDesc.substring(0, 50) + "..." : textOfDesc,
-            style: "font-size: 0.9em;"
-        });
-        goToScriptBtn = createElement("button", {
-            class: "goToScriptBtn",
-            onclick: "goToScript(" + scriptId + ")",
-            innerText: "calc"
-        });
 
         cardDiv.appendChild(titleP)
-        cardDiv.appendChild(descP)
-        cardDiv.appendChild(goToScriptBtn)
-        horizontalDivArray.push(cardDiv)
+        tabsWrapper.appendChild(cardDiv)
 
     }
-
-    let buf = 0;
-    rowDiv = createElement("div", {class: "row horizontalWrapper"});
-    for (let i = 0; i < horizontalDivArray.length; i += 1) {
-
-        if (buf < 2 && i !== horizontalDivArray.length - 1) rowDiv.appendChild(horizontalDivArray[i]);
-        else {
-            rowDiv.appendChild(horizontalDivArray[i])
-            placeHolder.appendChild(rowDiv)
-            rowDiv = createElement("div", {class: "row horizontalWrapper"});
-            buf = -1
-        }
-        buf += 1
-
-        // placeHolder.appendChild(horizontalDivArray[i])
-    }
-    /*for (let i = 0 ; i < rowDiv.length; i++) {
-    }*/
-    // placeHolder.appendChild(rowDiv)
 }
 
 function onAddNewScript() {
@@ -93,5 +66,5 @@ function onAddNewScript() {
 
 function goToScript(scriptID) {
     saveScriptId(scriptID);
-    window.location.href = "/script.html";
+    loadScriptData()
 }
